86b01bd7c944a4ac2f39cfb86042be7ead895ba6
t-walshe <thomas@walshe.tech>
2023-05-14 16:31:03 +0100

    [General] Structure project

---
+++ utils/_typing.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Callable,
+    Dict,
+    Hashable,
+    Iterator,
+    List,
+    Literal,
+    Mapping,
+    Optional,
+    Protocol,
+    Sequence,
+    Tuple,
+    Type as type_t,
+    TypeVar,
+    Union,
+)
+
+# Inspire by https://github.com/pandas-dev/pandas/blob/main/pandas/_typing.py
